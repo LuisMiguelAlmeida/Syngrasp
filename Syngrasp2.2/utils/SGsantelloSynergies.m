4b18729b0342f5fba21ec96a85122f0bb774e4c4
Lu+is Almeida <luis.mig.almeida96@gmail.com>
2019-07-19 19:17:24 +0100

    Added a new line of zeros on Smat to take into account the wrist joint

--- Syngrasp2.2/utils/SGsantelloSynergies.m
+++ Syngrasp2.2/utils/SGsantelloSynergies.m
@@ -3,7 +3,11 @@ function [qm, S] = SGsantello_synergies
 
 S_tmp = load('S_matrix.mat');
 S = S_tmp.S;
+
+s0 = zeros(1, size(S,2));% Taking into account wrist joint
+S = [s0; S];
 qm_tmp = load('qm.mat');
 qm = qm_tmp.qm;
+qm = [0, qm]; % Taking into account wrist joint
 
 
