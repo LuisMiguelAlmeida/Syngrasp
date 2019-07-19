fea44d3261bd722ab2c562009f46aa04f170817b
Lu+is Almeida <luis.mig.almeida96@gmail.com>
2019-07-19 19:23:41 +0100

    added 4 mm to cyl radius (for the hand not to enter the cylinder)

--- Syngrasp2.2/utils/SGlinkIntersection.m
+++ Syngrasp2.2/utils/SGlinkIntersection.m
@@ -15,7 +15,7 @@ function alpha = SGlinkIntersection(seg,struct2,epsilon)
             struct2_m = SGcube(struct2.Htr,struct2.dim(1)+offset,struct2.dim(2)+offset,struct2.dim(3)+offset);
             alpha = SGintSegCube(seg,struct2_m);
         case 'cyl'
-            struct2_m = SGcylinder(struct2.Htr,struct2.h,struct2.radius,struct2.res);
+            struct2_m = SGcylinder(struct2.Htr,struct2.h,struct2.radius+4,struct2.res);
             alpha = SGintSegCyl(seg,struct2_m,epsilon);
         case 'sph'
             struct2_m = SGsphere(struct2.Htr,struct2.radius,struct2.res);
