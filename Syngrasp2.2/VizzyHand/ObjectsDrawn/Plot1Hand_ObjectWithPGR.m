c3306100e4f05862792f2a54eb2a2c578e721c7d
Lu+is Almeida <luis.mig.almeida96@gmail.com>
2019-07-30 23:41:00 +0100

    Now this function receives a structure PGR with the heuristics computed

--- Syngrasp2.2/VizzyHand/ObjectsDrawn/Plot1Hand_ObjectWithPGR.m
+++ Syngrasp2.2/VizzyHand/ObjectsDrawn/Plot1Hand_ObjectWithPGR.m
@@ -1,7 +1,5 @@
-function [PCR,PGR_BF, PGR_H1, PGR_H2] = Plot1Hand_Object(hand, obj, titl, n_attached, n_fig)
+function Plot1Hand_ObjectWithPGR(hand, obj, titl, PGR, n_fig)
     % Plot the hand and an object with PCR, PGR brute force, H1 and H2
-    
-    PCR = 0;PGR_BF = 0;PGR_H1 = 0;PGR_H2 = 0;
     
     if ~isfield(hand, 'cp') || ~isfield(obj, 'cp')
         %error('No contact points');
@@ -21,11 +19,14 @@ function [PCR,PGR_BF, PGR_H1, PGR_H2] = Plot1Hand_Object(hand, obj, titl, n_atta
     ylabel('Y');
     zlabel('Z');
     title(titl);
-    [PCR]=SG_PCR(hand, obj);
-    [PGR_BF, ~, ~]=SG_PGRbruteforce(hand, obj); % Brute Force PGR
-    [PGR_H1, ~, ~]=SG_PGRh1(hand,obj); % PGR with Heuristic 1
-    [PGR_H2, ~, ~]=SG_PGRh2(hand,obj,n_attached); % PGR with Heuristic 2
-    str = sprintf('PCR: %.2f\nPGR Brute Borce: %.2f\nPGR H1: %.2f\nPGR H2: %.2f',PCR,PGR_BF, PGR_H1, PGR_H2);
+    
+    % Plot "Legend" with all PGR's given in struct "PGR"
+    PGRnames = fieldnames(PGR);
+    str = [];
+    for i=1:length(PGRnames)
+        PGRvalue = PGR.(PGRnames{i});
+        str = sprintf('%s%s: %.2f\n',str, PGRnames{i}, PGRvalue);
+    end
     text(0.9,0.9,str,'Units','normalized')
     hold off;
 end
