0d24b46a57f1efd9822f66c5a77020ee9bd0a453
Lu+is Almeida <luis.mig.almeida96@gmail.com>
2019-08-08 11:55:58 +0100

    Added python path for the GWS quality measure computation

--- Syngrasp2.2/install.m
+++ Syngrasp2.2/install.m
@@ -18,3 +18,20 @@ if 1
     end
     
 end
+
+% Load GWS python module and a shared library
+InstallPath = pwd;
+Module_path = [pwd, '/grasp_quality_metrics/Grasp_quality/GWSmetric/incremental_gws_lib/lib/1.0/examples'];
+SharedLib = [pwd, '/grasp_quality_metrics/Grasp_quality/GWSmetric/incremental_gws_lib/lib/1.0/x86_64'];
+
+
+
+if count(py.sys.path, Module_path) == 0
+    insert(py.sys.path,int32(0), Module_path);
+end
+
+if count(py.sys.path, SharedLib) == 0
+    insert(py.sys.path,int32(0), SharedLib);
+end
+
+py.importlib.import_module('incremental_gws_calculation'); % Adding library
